18f6a8f86b7cd560115bb7c40218b5f9376807b7
videvid <vidar@videvit.se>
2024-06-02 19:42:48 +0200

    meta data test

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,41 +1,47 @@
-'use client'
-import { useEffect, useState, Suspense } from "react";
+'use client';
+
+import { useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
 import { Inter } from "next/font/google";
-import Head from "next/head";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
-
-function DynamicTitle() {
-  const searchParams = useSearchParams();
-  const [pageTitle, setPageTitle] = useState("Vad finns i lådan");
-
-  useEffect(() => {
-    const title = searchParams.get("title");
-    if (title) {
-      setPageTitle(title);
-    }
-  }, [searchParams]);
-
-  return (
-    <Head>
-      <title>{pageTitle}</title>
-      <meta name="description" content="Kolla vad för roligt som finns här!" />
-    </Head>
-  );
-}
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const searchParams = useSearchParams();
+  const [pageTitle, setPageTitle] = useState("Vad finns i lådan");
+
+  useEffect(() => {
+    const title = searchParams.get("title");
+    if (title) {
+      console.log(title, 'title?');
+      setPageTitle(title);
+    }
+  }, [searchParams]);
+
+  useEffect(() => {
+    document.title = pageTitle;
+    const metaDescription = document.querySelector('meta[name="description"]');
+    if (metaDescription) {
+      metaDescription.setAttribute("content", "Kolla vad för roligt som finns här!");
+    } else {
+      const meta = document.createElement('meta');
+      meta.name = "description";
+      meta.content = "Kolla vad för roligt som finns här!";
+      document.head.appendChild(meta);
+    }
+  }, [pageTitle]);
+
   return (
     <html lang="en">
-      <Suspense fallback={<div>Loading...</div>}>
-        <DynamicTitle />
-      </Suspense>
+      <head>
+        <title>{pageTitle}</title>
+        <meta name="description" content="Kolla vad för roligt som finns här!" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
