c033bb6ebe8844b101126907c506590dc48b2c79
videvid <vidar@videvit.se>
2024-06-02 19:31:52 +0200

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,33 @@
-import type { Metadata } from "next";
+'use client'
+import { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
 import { Inter } from "next/font/google";
+import Head from "next/head";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Vad finns i lådan",
-  description: "Kolla vad för roligt som finns här!",
-};
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const searchParams = useSearchParams();
+  const [pageTitle, setPageTitle] = useState("Vad finns i lådan");
+
+  useEffect(() => {
+    const title = searchParams.get("title");
+    if (title) {
+      setPageTitle(title);
+    }
+  }, [searchParams]);
+
   return (
     <html lang="en">
+      <Head>
+        <title>{pageTitle}</title>
+        <meta name="description" content="Kolla vad för roligt som finns här!" />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
