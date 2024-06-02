819ad151c638ca9284321683999746083eefcc16
videvid <vidar@videvit.se>
2024-06-02 19:26:02 +0200

    a

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,21 @@
-import { Inter } from 'next/font/google';
-import './globals.css';
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
-export async function generateMetadata({ searchParams }: { searchParams: { title?: string } }) {
-  const title = searchParams ? searchParams.title : 'Vad finns här';
-  const description = 'Kolla vad för roligt som finns här!';
+export const metadata: Metadata = {
+  title: "Vad finns i lådan",
+  description: "Kolla vad för roligt som finns här!",
+};
 
-  return {
-    title,
-    description,
-  };
-}
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <html lang="en">
-      <head />
       <body className={inter.className}>{children}</body>
     </html>
   );
